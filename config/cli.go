1743c05bbdcd9a3ea6a04829f29da7de8071827a
Yakumo Saki <yakumo@ziomatrix.org>
2022-08-10 16:47:32 +0900

    fix pflag:help requested.

--- config/cli.go
+++ config/cli.go
@@ -30,11 +30,14 @@ func GetConfigFromCommandLine() *ConfigStruct {
 	var cliOption ConfigStruct
 
 	url := pflag.StringP("endpoint", "e", "", "Zabbix Server API endpoint url. example: http://192.168.0.20/api_jsonrpc.php")
+	zbxServer := pflag.StringP("zabbix", "z", "", "Zabbix Server hostname or IP address. overwrites endpoint url using http. if you want to use https, use -Z. example: 192.168.0.20")
+	zbxHttpsServer := pflag.StringP("zabbix-https", "Z", "", "Zabbix Server hostname or IP address. overwrites endpoint url using https. example: 192.168.0.20")
 	host := pflag.StringP("hostname", "s", "", "Zabbix Hostname")
 	key := pflag.StringP("key", "k", "", "Zabbix Item Key")
 	loglevel := pflag.StringP("loglevel", "l", "", "Loglevel TRACE>DEBUG>INFO>WARN>ERROR>FATAL")
 	output := pflag.StringP("output", "o", "", "Output type [JSON | VALUE]")
 	version := pflag.BoolP("version", "v", false, "Show version")
+	help := pflag.Bool("help", false, "Show this help")
 
 	setHelpMessage()
 	pflag.Parse()
@@ -44,12 +47,26 @@ func GetConfigFromCommandLine() *ConfigStruct {
 		os.Exit(0)
 	}
 
+	if *help || len(os.Args) == 0 {
+		pflag.Usage()
+		os.Exit(0)
+	}
+
 	// todo get from .env -> ~/.config/zabbix-getter.conf -> cli option
 	cliOption.Url = *url
 	cliOption.Hostname = *host
 	cliOption.Key = *key
 	cliOption.Loglevel = *loglevel
 	cliOption.Output = *output
+
+	switch {
+	case zbxServer != nil && zbxHttpsServer != nil:
+		panic("can not use both -z and -zs.")
+	case zbxServer != nil:
+		cliOption.Url = fmt.Sprintf("http://%s/api_jsonrpc.php", *zbxServer)
+	case zbxHttpsServer != nil:
+		cliOption.Url = fmt.Sprintf("https://%s/api_jsonrpc.php", *zbxHttpsServer)
+	}
 
 	if cliOption.Loglevel != "" {
 		ylog.SetLogLevel(cliOption.Loglevel)
