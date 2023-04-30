b7247c873ea01c0249ae0c588dc4010162751624
Yakumo Saki <yakumo@ziomatrix.org>
2023-05-01 01:40:22 +0900

    #2 support login api > ver5.4

--- zabbix/Authenticate.go
+++ zabbix/Authenticate.go
@@ -23,15 +23,24 @@ type zabbixError struct {
 	Data    string
 }
 
-// Authenticate to zabbix and get authenticate token
 func Authenticate(url string, username string, password string) (string, error) {
+	result, errcode, err := AuthenticateAfter54(url, username, password)
+	if errcode == -32602 {
+		// zabbix 5.4以前
+		result, _, err = AuthenticateBefore54(url, username, password)
+	}
+
+	return result, err
+}
+
+// Authenticate to zabbix and get authenticate token
+func AuthenticateAfter54(url string, username string, password string) (string, int, error) {
 	var logger = ylog.GetLogger()
 
-	jsonTemplate := `
-	{"jsonrpc":"2.0","method":"user.login","params":
-	{"user":"%s","password":"%s"},
-	"id":1,"auth":null}`
+	// zabbix <= 5.2 username is user, zabbix 5.4 or newer, username params is username
+	jsonTemplate := `{"jsonrpc":"2.0","method":"user.login","params":{"username":"%s","password":"%s"},"id":1}`
 	jsonStr := fmt.Sprintf(jsonTemplate, username, password)
+	logger.T("Sending", jsonStr) // htmlをstringで取得
 
 	req, _ := http.NewRequest("POST", url, bytes.NewBuffer([]byte(jsonStr)))
 	req.Header.Set("Content-Type", "application/json")
@@ -39,27 +48,67 @@ func Authenticate(url string, username string, password string) (string, error) 
 	client := new(http.Client)
 	resp, err := client.Do(req)
 	if err != nil {
-		return "", &ZabbixError{Msg: "Error while API request. (user.login)", Err: err}
+		return "", -1, &ZabbixError{Msg: "Error while API request. (user.login)", Err: err}
 	}
 
 	// expected result
 	// {"jsonrpc":"2.0","result":"057466f9a6cb65b3d57d9460cc792b9b","id":1}
 	byteArray, _ := io.ReadAll(resp.Body)
 	resp.Body.Close()
-	logger.T(string(byteArray)) // htmlをstringで取得
+	logger.T("Response", string(byteArray)) // htmlをstringで取得
 
 	// parse JSON
 	var decode_data authenticateResult
 	if err := json.Unmarshal(byteArray, &decode_data); err != nil {
-		return "", err
+		return "", -1, err
 	}
 
 	// check authorize success
 	if decode_data.Error.Code != 0 {
-		return "", fmt.Errorf("login failed: error %d %s", decode_data.Error.Code, decode_data.Error.Data)
+		return "", -1, fmt.Errorf("login failed: error %d %s", decode_data.Error.Code, decode_data.Error.Data)
 	}
 
 	// 表示
 	// logger.D(decode_data.Result)
-	return decode_data.Result, nil
+	return decode_data.Result, 0, nil
+}
+
+// Authenticate to zabbix and get authenticate token
+func AuthenticateBefore54(url string, username string, password string) (string, int, error) {
+	var logger = ylog.GetLogger()
+
+	// zabbix <= 5.2 username is user, zabbix 5.4 or newer, username params is username
+	jsonTemplate := `{"jsonrpc":"2.0","method":"user.login","params":{"user":"%s","password":"%s"},"id":1, auth: null}`
+	jsonStr := fmt.Sprintf(jsonTemplate, username, password)
+	logger.T("Sending", jsonStr) // htmlをstringで取得
+
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer([]byte(jsonStr)))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", -1, &ZabbixError{Msg: "Error while API request. (user.login)", Err: err}
+	}
+
+	// expected result
+	// {"jsonrpc":"2.0","result":"057466f9a6cb65b3d57d9460cc792b9b","id":1}
+	byteArray, _ := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	logger.T("Response", string(byteArray)) // htmlをstringで取得
+
+	// parse JSON
+	var decode_data authenticateResult
+	if err := json.Unmarshal(byteArray, &decode_data); err != nil {
+		return "", -1, err
+	}
+
+	// check authorize success
+	if decode_data.Error.Code != 0 {
+		return "", -1, fmt.Errorf("login failed: error %d %s", decode_data.Error.Code, decode_data.Error.Data)
+	}
+
+	// 表示
+	// logger.D(decode_data.Result)
+	return decode_data.Result, 0, nil
 }
